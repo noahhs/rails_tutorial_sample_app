4b57f7b51cfd4752a0066e8811fa85b778708805
Noah <noahhs@noahhs-VirtualBox.(none)>
2013-05-07 05:11:21 -0700

    Finish user edit, update, index, and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,9 +26,20 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
+  it { should respond_to(:admin) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
 
   describe "when password is not present" do
     before { @user.password = @user.password_confirmation = " " }
@@ -59,7 +70,6 @@ describe User do
     before { @user.name = "a" * 51 }
     it { should_not be_valid }
   end
-
 
   describe "when email format is invalid" do
     it "should be invalid" do
